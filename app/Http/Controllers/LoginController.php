8fe90225c5da0d8fe5c78c9b32236a97539552ff
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2024-05-15 21:22:22 +0700

    Modify LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -23,9 +23,14 @@ class LoginController extends Controller
         $credentials = $request->only('nim', 'password');
 
         if (Auth::attempt($credentials)) {
-            return redirect('/dashboard');
+            $user = Auth::user();
+            session()->put('user.nama', $user->nama);
+            session()->put('user.nim', $user->nim);
+            session()->put('user.kelas', $user->kelas);
+            return redirect('/dashboard_user');
         } else {
             return redirect('login')->with('failed', 'No Induk atau Password Salah');
         }
+        
     }
 }
