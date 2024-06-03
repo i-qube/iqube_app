e702d0b3d470b13ef3d1805936787ab56794c6d7
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2024-06-04 03:49:13 +0700

    modify form view

--- resources/views/user/item/item_borrow.blade.php
+++ resources/views/user/item/item_borrow.blade.php
@@ -84,8 +84,8 @@
                         </div>
                         <div class="w-2/4 pr-2 relative">
                             {{-- <label for="quantity" class="text-black pl-1">Id</label> --}}
-                            <input type="text" id="selection" name="item_id" readonly
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-1/6 p-2.5 pl-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <input type="text" id="selection" name="item_id" readonly hidden>
+                                {{-- class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-1/6 p-2.5 pl-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> --}}
                         </div>
                     </div>
                 </div>
